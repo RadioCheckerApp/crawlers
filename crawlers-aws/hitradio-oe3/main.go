eb3f3ca0d01d3e75735b6bd3f3193c1689a54006
IggyBlob <socialmedia@paulhaunschmied.com>
2018-09-01 15:13:24 +0200

    Implement crawler function hitradio-oe3

---
+++ crawlers-aws/hitradio-oe3/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/RadioCheckerApp/crawlers/crawler"
+	"github.com/RadioCheckerApp/crawlers/fetcher"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"os"
+)
+
+func Handler(event events.CloudWatchEvent) error {
+	log.Println("INFO:    Crawler triggered.")
+	defer log.Println("INFO:    Crawler finshed.")
+
+	twitterConsumerKey := os.Getenv("TWITTER_CONSUMER_KEY")
+	twitterConsumerKeySecret := os.Getenv("TWITTER_CONSUMER_KEY_SECRET")
+	twitterOauthAccessToken := os.Getenv("TWITTER_OAUTH_ACCESS_TOKEN")
+	twitterOauthAccessTokenSecret := os.Getenv("TWITTER_OAUTH_ACCESS_TOKEN_SECRET")
+
+	stationId := os.Getenv("STATION_ID_HITRADIO_OE3")
+	rcAPIHost := os.Getenv("RC_API_HOST")
+	rcAPIStage := os.Getenv("RC_API_STAGE")
+	rcAPIKey := os.Getenv("RC_API_KEY")
+
+	oe3Fetcher, err := fetcher.NewHitradioOE3Fetcher(
+		twitterConsumerKey,
+		twitterConsumerKeySecret,
+		twitterOauthAccessToken,
+		twitterOauthAccessTokenSecret,
+	)
+	if err != nil {
+		log.Printf("ERROR:  Unable to create HitradioOE3Fetcher. Message: `%s`.", err.Error())
+		return err
+	}
+
+	homebase := crawler.HomeBaseConnector{rcAPIHost, rcAPIStage, rcAPIKey}
+
+	oe3Crawler, err := crawler.NewCrawler(stationId, oe3Fetcher, homebase)
+	if err != nil {
+		log.Printf("ERROR:  Unable to create crawler for station `%s`. Message: `%s`.",
+			stationId, err.Error())
+		return err
+	}
+
+	oe3Crawler.Crawl()
+
+	return nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
